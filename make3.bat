954159affef128b971df83badc33a448cf77426d
stubbscroll <spaans@pvv.org>
2020-06-24 20:32:37 +0200

    add plank to makefile

---
+++ make3.bat
@@ -0,0 +1,4 @@
+gcc -o plank-bfsd.exe bfsd.c plank.c -O3 -Wall -std=c99
+gcc -o plank-bfsdu.exe bfsdu.c plank.c -O3 -Wall -std=c99
+gcc -o plank-bfs.exe bfs.c plank.c -O3 -Wall -std=c99
+gcc -o plank-bfs2.exe bfs2.c plank.c -O3 -Wall -std=c99
